2e45dba660234ff4cce40e18668c962f7309b9cd
Thorsten Lorenz <thlorenz@gmx.de>
2013-03-17 19:10:16 -0400

    documentation

--- README.md
+++ README.md
@@ -16,3 +16,15 @@ browserify()
 
 Although this example shows how to use this while building a bundle with browserify, show-stream-progress works with any
 stream.
+
+## API
+
+```
+/**
+ * @name showProgress
+ * @function
+ * @param out {Stream} The stream to output progress indicators to, defaults to process.stdout.
+ * @return {Stream} Readable and writable stream that just passes along data written to it.
+ */
+```
+
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 'use strict';
 var through = require('through');
 
-module.exports = function (out) {
+/**
+ * @name showProgress
+ * @function
+ * @param out {Stream} The stream to output progress indicators to, defaults to process.stdout.
+ * @return {Stream} Readable and writable stream that just passes along data written to it.
+ */
+module.exports = function showProgress(out) {
   var data = '';
   out = out || process.stdout;
 
