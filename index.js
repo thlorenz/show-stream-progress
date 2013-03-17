7da7a7abc18ae03d3d03ca2c9be952e00902e550
Thorsten Lorenz <thlorenz@gmx.de>
2013-03-17 18:05:23 -0400

    reporiting when file is completely done

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ var through = require('through');
 
 module.exports = function (file) {
   var data = '';
-  process.stderr.write(file + ' : ' );
 
   return through(write, end);
 
   function write (buf) { data += buf; }
   function end () {
+    process.stderr.write(file + ' : ' );
     this.queue(data);
     this.queue(null);
   }
